e8775362f53bf0e9a0c4206884206cb2864c52d8
migo3ol <166726636+migo3ol@users.noreply.github.com>
2025-04-25 10:32:53 +0800

    e

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMJ Furnishings</title>
 
-     <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 
@@ -18,7 +18,7 @@
 <!-- Footer -->
 <footer class="footer">
     <div class="container py-4">
-    <div class="row text-center text-md-start align-items-start">
+        <div class="row text-center text-md-start align-items-start">
             <!-- Logo Section -->
             <div class="col-12 col-md-3 mb-3">
                 <img src="assets/navbar-logo1.png" class="footer-logo img-fluid" alt="SMJ Logo">
@@ -27,10 +27,9 @@
             <!-- Company Info -->
             <div class="col-12 col-md-3 mb-3">
                 <h5>Company</h5>
-                <p class="mb-1 lh-lg">Salcedo Village, LGO1,<br> Herrera Tower Condo,<br>98 V.A. Rufino St, Cor. <br> 1227 Valero, Makati, Metro Manila</p>
-                <p class="mb-1 lh-lg"># 893-3337/ 813-3972</p>
+                <p class="mb-1 lh-lg">Salcedo Village, LGO1,<br> Herrera Tower Condo,<br>98 V.A. Rufino St, Cor.<br>1227 Valero, Makati, Metro Manila</p>
+                <p class="mb-1 lh-lg"># 893-3337 / 813-3972</p>
                 <p class="mb-1 lh-lg">+63 917 847 6287 <br> +63 925 535 1381</p>
-                
             </div>
 
             <!-- Socials -->
@@ -46,15 +45,33 @@
             <div class="col-12 col-md-3 mb-3">
                 <h5>About</h5>
                 <p><a href="#">Terms & Conditions</a></p>
-                <p><a href="#">Privacy Policy</a></p>
+                <p><a href="#" data-bs-toggle="modal" data-bs-target="#securityPolicyModal">Security Policy Statement</a></p>
             </div>
         </div>
     </div>
+
     <!-- Copyright -->
     <div class="footer-bottom text-center py-2">
         Copyright © 2025 SMJ Furnishings Philippines Inc. All rights reserved
     </div>
 </footer>
+
+<!-- Security Policy Modal -->
+<div class="modal fade" id="securityPolicyModal" tabindex="-1" aria-labelledby="securityPolicyModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title w-100 text-center" id="securityPolicyModalLabel">SMJ Philippines Security Policy Statement</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body" style="text-align: justify;">
+        <p>
+        SMJ will take reasonable efforts to protect our supply chain, including data in our possession or our control, by making reasonable security arrangements to prevent unauthorised access, collection, use, disclosure, copying, modification, disposal or similar risks; in the course of our business. However, we cannot completely guarantee the security of any Personal Data we may have collected from or about you, or that for example no harmful code will enter our website (for example viruses, bugs, trojan horses, spyware or adware).
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
